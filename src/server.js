bb5f451c2a2ca5d37359ef1a045750ed7adff317
Tien Tan <tanquoctien@gmail.com>
2016-12-24 22:44:02 +0700

    Add forever package for node server.

--- src/server.js
+++ src/server.js
@@ -1,14 +1,8 @@
 var express = require('express');
-var app = express();
 var path = require('path');
-var compression = require('compression');
+var app = express();
 
-app.use(compression({ threshold: 0 }));
-app.use(express.static(path.join(__dirname, '../dist'), {
-  setHeaders: function (res) {
-    res.setHeader('Content-Encoding', 'gzip');
-  }
-}));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
