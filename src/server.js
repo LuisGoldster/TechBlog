0ffeddcec034c956e09725da917d1634d99c56cb
Tien Tan <tanquoctien@gmail.com>
2016-12-25 12:16:14 +0700

    Improve es6 syntax in server.js

--- src/server.js
+++ src/server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var compression = require('compression');
+let express     = require('express'),
+    path        = require('path'),
+    compression = require('compression');
 
-var app = express();
+let app = express();
 app.use(compression({ threshold: 0 }));
 app.use(express.static(path.join(__dirname, '../dist')));
-app.get('/*', function(req, res){
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '../dist/index.html'));
 });
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 })
