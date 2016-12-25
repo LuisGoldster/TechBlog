beb71c816b1cee5f3152f7d4728017891dde930d
Tien Tan <tanquoctien@gmail.com>
2016-12-25 07:55:06 +0700

    Add compression for testing (will remove later).

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var path = require('path');
+var compression = require('compression');
 var app = express();
 
+app.use(compression({ threshold: 0 }));
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(3000, function () {
