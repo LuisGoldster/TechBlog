505c6469aa476315da1d3a35c9edadbe2327d19b
Tien Tan <tanquoctien@gmail.com>
2016-12-25 11:57:12 +0700

    Re-route all routes to index.html.

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var path = require('path');
 var compression = require('compression');
-var app = express();
 
+var app = express();
 app.use(compression({ threshold: 0 }));
 app.use(express.static(path.join(__dirname, '../dist')));
+app.get('/*', function(req, res){
+  res.sendFile(path.join(__dirname, '../dist/index.html'));
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
