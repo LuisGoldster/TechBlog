267ccd5a543e1efd076ed184d1ff1877a7da8b29
Tien Tan <tanquoctien@gmail.com>
2016-12-30 15:28:04 +0700

    Minor change for routing.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,8 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
-import { NoContentComponent } from './pages/no-content/no-content.component';
+import { LoginComponent } from './pages/login/index'
+import { NoContentComponent } from './pages/no-content/index';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: '**', component: NoContentComponent }
 ];
 
